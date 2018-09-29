478b00e55a8efca997caeaa50d4aa1b84b338db6
Jose Zamora Orellana <jzamoraorellana@joses-mbp.lehman.edu>
2018-09-29 17:20:31 -0400

    Initial Commit

---
+++ Set/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Set
+//
+//  Created by Jose Zamora Orellana on 9/29/18.
+//  Copyright © 2018 Jose Zamora Orellana. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
